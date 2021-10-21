5dcbbe82bd5c5503fb81e1c43f5082ac20c1ea3d
alex-shepel <79785130+alex-shepel@users.noreply.github.com>
2021-10-21 12:34:25 +0000

    Deploying to gh-pages from @ alex-shepel/dolphi-ice-cream@c40856d63f2a7003c7a7550947e456d3c620c003 🚀

--- src.00c2af43.js
+++ src.00c2af43.js
@@ -5,4 +5,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./js/mobile-menu");
 },{"./sass/main.scss":"clu1","./js/mobile-menu":"sByI"}]},{},["Focm"], null)
-//# sourceMappingURL=/dolphi-ice-cream/src.7f44babf.js.map
+//# sourceMappingURL=/dolphi-ice-cream/src.00c2af43.js.map
