943b55161025dec6c032a10ae0f1c4649c41b254
Tetiana Zykova <tatiana.nv96@gmail.com>
2021-10-21 11:48:26 +0300

    add js for modal-products

---
+++ src/js/modal-products.js
@@ -0,0 +1,43 @@
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-products-pink-open]'),
+    closeModalBtn: document.querySelector('[data-modal-products-pink-close]'),
+    modal: document.querySelector('[data-modal-products-pink]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop-products--is-hidden');
+  }
+})();
+
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-products-orange-open]'),
+    closeModalBtn: document.querySelector('[data-modal-products-orange-close]'),
+    modal: document.querySelector('[data-modal-products-orange]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop-products--is-hidden');
+  }
+})();
+(() => {
+  const refs = {
+    openModalBtn: document.querySelector('[data-modal-products-green-open]'),
+    closeModalBtn: document.querySelector('[data-modal-products-green-close]'),
+    modal: document.querySelector('[data-modal-products-green]'),
+  };
+
+  refs.openModalBtn.addEventListener('click', toggleModal);
+  refs.closeModalBtn.addEventListener('click', toggleModal);
+
+  function toggleModal() {
+    refs.modal.classList.toggle('backdrop-products--is-hidden');
+  }
+})();
